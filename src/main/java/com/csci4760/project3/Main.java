1126eb9d9f00008e9f86282789923c59cf6355e7
JohnLavender474 <jml62608@uga.edu>
2023-04-26 22:57:52 -0400

    Running program

--- src/main/java/com/csci4760/project3/Main.java
+++ src/main/java/com/csci4760/project3/Main.java
@@ -1,21 +1,31 @@
 package com.csci4760.project3;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    private static final String SAMPLE_TCP_DUMP_1 = "SampleTcpDump_1.txt";
-    private static final String SAMPLE_TCP_DUMP_2 = "SampleTcpDump_2.txt";
-    private static final String TCP_DUMP = "TcpDump.txt";
-
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter file path: ");
-        String src = TCP_DUMP; // scanner.nextLine();
+        System.out.print("Enter path to tcp dump: ");
+        String src = scanner.nextLine();
+        System.out.println();
+        System.out.print("Enter output file name: ");
+        String output = scanner.nextLine();
         System.out.println();
         List<TraceRouteToken> tokens = RouteTracer.traceRoute(src);
-        tokens.forEach(token -> System.out.println(token + "\n"));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(output));
+        tokens.forEach(token -> {
+            try {
+                writer.write(token + "\n");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        writer.close();
     }
 
 }
